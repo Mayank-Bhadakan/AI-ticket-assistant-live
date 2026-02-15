595d1e3848e48be14afe4e93db5533c15fdf1d46
Mayank Bhadakan <mayankbhadakan95@gmail.com>
2026-02-15 01:14:32 -0800

    Connect inggest production

--- ai-ticket-assistant/inngest/client.js
+++ ai-ticket-assistant/inngest/client.js
@@ -1,3 +1,6 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "ai-ticket-app" });
+export const inngest = new Inngest({ 
+    id: "ai-ticket-app",
+    eventKey: process.env.INNGEST_EVENT_KEY,
+});
